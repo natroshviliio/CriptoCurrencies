cd8a891ca689cdda916d6b4453c74265361b9329
Ioane <38262019+ioane-stacks@users.noreply.github.com>
2022-07-17 21:03:31 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const url = 'https://api.coingecko.com/api/v3/coins/';
+const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=';
+const currency = 'usd';
 let dataArr = [];
 let converterCurrency = {
 	coinName: '',
@@ -13,38 +14,46 @@ const cb = document.getElementById('cb');
 const prevBtn = document.querySelector('.prev');
 const nextBtn = document.querySelector('.next');
 (async () => {
-	let response = await fetch(url);
+	let response = await fetch(url + currency);
 	let data = await response.json();
 	for (let d of data) {
 		dataArr.push(d);
 	}
-	dataArr.sort((curr, next) => curr.market_data.market_cap_rank - next.market_data.market_cap_rank, 0);
-	const topCurrData = dataArr.filter((_, i) => i < 10);
-	console.log(topCurrData);
-	topCurrData.map((d, i) => {
-		const { name, symbol, market_data, image } = d;
-		const { low_24h, high_24h, current_price } = market_data;
+	const topCurrData = dataArr.filter((d) => d.market_cap_rank <= 10);
+	topCurrData.map((d) => {
+		const {
+			name,
+			symbol,
+			image,
+			low_24h,
+			high_24h,
+			current_price,
+			price_change_percentage_24h,
+			price_change_24h,
+			market_cap_rank,
+		} = d;
 		currencyBoard.insertAdjacentHTML(
 			'beforeend',
 			addTopCurrencies(
 				name,
 				symbol,
-				current_price.usd,
-				current_price.eur,
-				image.large,
-				low_24h.usd,
-				high_24h.usd,
-				i,
+				current_price,
+				price_change_percentage_24h.toFixed(4),
+				price_change_24h.toFixed(4),
+				image,
+				low_24h,
+				high_24h,
+				market_cap_rank,
 			),
 		);
 	});
-	const temp = dataArr.filter((d) => d.market_data.market_cap_rank === 1)[0];
+	const temp = dataArr.filter((d) => d.market_cap_rank === 1)[0];
 	converterCurrency = {
 		coinName: temp.name,
 		coinSymbol: temp.symbol.toUpperCase(),
-		coinValue: 1 / temp.market_data.current_price.usd,
+		coinValue: 1 / temp.current_price,
 		currSymbol: 'USD',
-		currValue: temp.market_data.current_price.usd,
+		currValue: temp.current_price,
 	};
 	addConverter(converterCurrency);
 })();
@@ -99,21 +108,22 @@ const inputCur = document.getElementById('inputcur');
 
 //CONVERTER
 inputCoin.addEventListener('keyup', () => {
-	const currPrice = dataArr.find((d) => d.id === 'bitcoin').market_data.current_price.usd;
+	const currPrice = dataArr.find((d) => d.id === 'bitcoin').current_price;
 	inputCur.value = (inputCoin.value * currPrice).toFixed(3);
 });
 
 inputCur.addEventListener('keyup', () => {
-	const currPrice = dataArr.find((d) => d.id === 'bitcoin').market_data.current_price.usd;
+	const currPrice = dataArr.find((d) => d.id === 'bitcoin').current_price;
 	inputCoin.value = (inputCur.value * (1 / currPrice)).toFixed(6);
 });
 
-function addTopCurrencies(name, abr, curUS, curEUR, iconUrl, min, max, count) {
+function addTopCurrencies(name, abr, curUS, pcp_24h, pc_24h, iconUrl, min, max, count) {
+	const priceChangeP = pcp_24h >= 0 ? `+${pcp_24h}` : pcp_24h;
+	const priceChange = pc_24h >= 0 ? `+${pc_24h}` : pc_24h;
 	const topcurr = `
 		<div class="topcur">
 			<div class="choiceoper">
-				<a href="#">Buy</a>
-				<a href="#">Exchange</a>
+				<a href="#">more</a>
 			</div>
             <div class="topcur-header">
                 <div class="topcur-header-img" style="background-image:url('${iconUrl}')"></div>
@@ -122,7 +132,10 @@ function addTopCurrencies(name, abr, curUS, curEUR, iconUrl, min, max, count) {
             </div>
             <div class="topcur-body">
                 <h1>$${curUS}</h1>
-                <h1>€${curEUR}</h1>
+				<div class="p-change ${pcp_24h >= 0 ? 'progress' : 'degress'}">
+					<p>${priceChangeP}%</p>
+					<span>${priceChange}$</span>
+				</div>
             </div>
 			<div class="topcur-foot">
 				<div class="topcur-foot-inf max">
@@ -134,7 +147,7 @@ function addTopCurrencies(name, abr, curUS, curEUR, iconUrl, min, max, count) {
                     <span>min | 24h</span>
                 </div>
 			</div>
-			<div class="topcur-pager"><p>${count + 1}</p></div>
+			<div class="topcur-pager"><p>${count}</p></div>
         </div>`;
 
 	return topcurr;
