e14872f5510fcb74cc6de55ac0a3ec7af1b55921
Ioane <38262019+ioane-stacks@users.noreply.github.com>
2022-07-19 00:59:27 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=';
-const currency = 'usd';
-let dataArr = [];
+let dataApi = {
+	url: 'https://api.coingecko.com/api/v3/coins/markets',
+	currency: 'usd',
+	page: 1,
+	get: function (url = this.url, currency = this.currency, page = this.page) {
+		return `${url}?vs_currency=${currency}&page=${page}&price_change_percentage=1h`;
+	},
+};
+
 let converterCurrency = {
 	coinName: '',
 	coinSymbol: '',
@@ -9,18 +15,25 @@ let converterCurrency = {
 	currValue: 0,
 };
 
+let topCurrencies = [];
+
+let currencies = ['USD', 'AUD', 'EUR', 'CAD'];
+let converterTop10 = [];
+let converterSearchCur = [];
+
 const currencyBoard = document.querySelector('.currency-board');
 const cb = document.getElementById('cb');
 const prevBtn = document.querySelector('.prev');
 const nextBtn = document.querySelector('.next');
+
 (async () => {
-	let response = await fetch(url + currency);
+	let response = await fetch(dataApi.get());
 	let data = await response.json();
 	for (let d of data) {
-		dataArr.push(d);
+		d.market_cap_rank <= 10 ? topCurrencies.push(d) : '';
 	}
-	const topCurrData = dataArr.filter((d) => d.market_cap_rank <= 10);
-	topCurrData.map((d) => {
+	converterTop10 = topCurrencies;
+	topCurrencies.map((d) => {
 		const {
 			name,
 			symbol,
@@ -38,8 +51,8 @@ const nextBtn = document.querySelector('.next');
 				name,
 				symbol,
 				current_price,
-				price_change_percentage_24h.toFixed(4),
-				price_change_24h.toFixed(4),
+				price_change_percentage_24h.toFixed(3) == 0 ? 0 : price_change_percentage_24h.toFixed(3),
+				price_change_24h.toFixed(3) == 0 ? 0 : price_change_24h.toFixed(3),
 				image,
 				low_24h,
 				high_24h,
@@ -47,7 +60,7 @@ const nextBtn = document.querySelector('.next');
 			),
 		);
 	});
-	const temp = dataArr.filter((d) => d.market_cap_rank === 1)[0];
+	const temp = converterTop10.filter((d) => d.market_cap_rank === 1)[0];
 	converterCurrency = {
 		coinName: temp.name,
 		coinSymbol: temp.symbol.toUpperCase(),
@@ -55,7 +68,6 @@ const nextBtn = document.querySelector('.next');
 		currSymbol: 'USD',
 		currValue: temp.current_price,
 	};
-	addConverter(converterCurrency);
 })();
 
 const loginButton = document.getElementById('logn');
@@ -105,21 +117,82 @@ cb.addEventListener('mousemove', (e) => {
 
 const inputCoin = document.getElementById('inputcoin');
 const inputCur = document.getElementById('inputcur');
+inputCoin.min = 0;
+inputCur.min = 0;
+let inputCoinValue;
+let inputCurValue;
 
 //CONVERTER
-inputCoin.addEventListener('keyup', () => {
-	const currPrice = dataArr.find((d) => d.id === 'bitcoin').current_price;
+inputCoin.addEventListener('keydown', (e) => {
+	if (e.key === '-' || e.key === '+' || e.key === 'e' || e.key === 'E') {
+		e.preventDefault();
+	}
+});
+inputCur.addEventListener('keydown', (e) => {
+	if (e.key === '-' || e.key === '+' || e.key === 'e') {
+		e.preventDefault();
+	}
+});
+inputCoin.addEventListener('keyup', (e) => {
+	const currPrice = converterTop10.find((d) => d.id === 'bitcoin').current_price;
+
+	inputCoin.value === '' && e.key === 'Backspace'
+		? (inputCoinValue = '')
+		: inputCoin.value !== ''
+		? (inputCoinValue = inputCoin.value)
+		: '';
+
+	inputCoin.value = inputCoinValue;
+
 	inputCur.value = (inputCoin.value * currPrice).toFixed(3);
 });
 
-inputCur.addEventListener('keyup', () => {
-	const currPrice = dataArr.find((d) => d.id === 'bitcoin').current_price;
+inputCur.addEventListener('keyup', (e) => {
+	const currPrice = converterTop10.find((d) => d.id === 'bitcoin').current_price;
+
+	inputCur.value === '' && e.key === 'Backspace'
+		? (inputCurValue = '')
+		: inputCur.value !== ''
+		? (inputCurValue = inputCur.value)
+		: '';
+
+	inputCur.value = inputCurValue;
+
 	inputCoin.value = (inputCur.value * (1 / currPrice)).toFixed(6);
 });
 
+setInterval(() => {
+	const fetchData = async () => {
+		const response = await fetch(dataApi.get(dataApi.url, dataApi.currency, 1));
+		const data = await response.json();
+		console.log(data[0].price_change_percentage_1h_in_currency);
+	};
+	fetchData();
+}, 1000);
+
+//JSX
 function addTopCurrencies(name, abr, curUS, pcp_24h, pc_24h, iconUrl, min, max, count) {
-	const priceChangeP = pcp_24h >= 0 ? `+${pcp_24h}` : pcp_24h;
-	const priceChange = pc_24h >= 0 ? `+${pc_24h}` : pc_24h;
+	let priceChangeP = pcp_24h > 0 ? `+${pcp_24h}` : pcp_24h;
+	let priceChange = pc_24h > 0 ? `+${pc_24h}` : pc_24h;
+	let currentColor;
+	if (pcp_24h > 0) {
+		priceChangeP = `+${pcp_24h}%`;
+		currentColor = 'progress';
+	} else if (pcp_24h < 0) {
+		priceChangeP = `${pcp_24h}$`;
+		currentColor = 'degress';
+	} else {
+		priceChangeP = ``;
+	}
+	if (pc_24h > 0) {
+		priceChange = `+${pc_24h}$`;
+		currentColor = 'progress';
+	} else if (pc_24h < 0) {
+		priceChange = `${pc_24h}$`;
+		currentColor = `degress`;
+	} else {
+		priceChange = ``;
+	}
 	const topcurr = `
 		<div class="topcur">
 			<div class="choiceoper">
@@ -132,9 +205,9 @@ function addTopCurrencies(name, abr, curUS, pcp_24h, pc_24h, iconUrl, min, max, 
             </div>
             <div class="topcur-body">
                 <h1>$${curUS}</h1>
-				<div class="p-change ${pcp_24h >= 0 ? 'progress' : 'degress'}">
-					<p>${priceChangeP}%</p>
-					<span>${priceChange}$</span>
+				<div class="p-change ${currentColor}">
+					<p>${priceChangeP}</p>
+					<span>${priceChange}</span>
 				</div>
             </div>
 			<div class="topcur-foot">
@@ -154,5 +227,22 @@ function addTopCurrencies(name, abr, curUS, pcp_24h, pc_24h, iconUrl, min, max, 
 }
 
 function addConverter({ coinName, coinSymbol, coinValue, currSymbol, currValue }) {
-	console.log(coinName, coinSymbol, coinValue, currSymbol, currValue);
+	const converter = `
+	<div class="converter-icon"></div>
+    <div class="converter-header">
+        <h3>1 BTC = 20239 USD</h3>
+    </div>
+    <div class="converter-header">
+        <h3>1 USD = 0.00004715 BTC</h3>
+    </div>
+    <div class="converter-body">
+        <div class="input-group">
+            <label id="convcoin" for="inputcoin">BTC<span>▼</span></label>
+            <input id="inputcoin" type="number">
+        </div>
+        <div class="input-group">
+            <label id="convcur" for="inputcur">USD<span>▼</span></label>
+            <input id="inputcur" type="number">
+        </div>
+    </div>`;
 }
