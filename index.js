46ad804aeafcd0a9fdf89131ec0fb609c04df3ed
Ioane <38262019+ioane-stacks@users.noreply.github.com>
2022-07-22 04:44:09 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -14,12 +14,13 @@ let dataApi = {
 	},
 };
 
-let converterCurrency = {
+let converterCurrentCurrency = {
 	coinName: '',
 	coinSymbol: '',
 	coinValue: 0,
 	currSymbol: '',
 	currValue: 0,
+	coinIco: '',
 };
 
 let topCurrencies = [];
@@ -29,6 +30,7 @@ let converterTop10 = [];
 let converterSearchCur = [];
 
 const currencyBoard = document.querySelector('.currency-board');
+const converterBoard = document.querySelector('.converter-board');
 const cb = document.getElementById('cb');
 const prevBtn = document.querySelector('.prev');
 const nextBtn = document.querySelector('.next');
@@ -70,13 +72,76 @@ const nextBtn = document.querySelector('.next');
 		);
 	});
 	const temp = converterTop10.filter((d) => d.market_cap_rank === 1)[0];
-	converterCurrency = {
+
+	converterCurrentCurrency = {
 		coinName: temp.name,
 		coinSymbol: temp.symbol.toUpperCase(),
 		coinValue: 1 / temp.current_price,
 		currSymbol: 'USD',
 		currValue: temp.current_price,
+		coinIco: temp.image,
+		sparkline: temp.sparkline_in_7d.price,
 	};
+
+	converterBoard.insertAdjacentHTML('afterbegin', addConverter(converterCurrentCurrency));
+	(() => {
+		const inputCoin = document.getElementById('inputcoin');
+		const inputCur = document.getElementById('inputcur');
+		inputCoin.min = 0;
+		inputCur.min = 0;
+
+		//CONVERTER
+		inputCoin.addEventListener('keydown', (e) => {
+			if (e.key === '-' || e.key === '+' || e.key === 'e' || e.key === 'E') {
+				e.preventDefault();
+			}
+		});
+		inputCur.addEventListener('keydown', (e) => {
+			if (e.key === '-' || e.key === '+' || e.key === 'e') {
+				e.preventDefault();
+			}
+		});
+		inputCoin.addEventListener('keyup', (e) => {
+			//#region THIS IS A RESTRICTION FOR MOBILE DEVICES BUT NEED TO FIX SOMETHING GETTING WRONG
+			// inputCoin.value === '' && e.key === 'Backspace'
+			// 	? (inputCoinValue = '')
+			// 	: inputCoin.value !== ''
+			// 	? (inputCoinValue = inputCoin.value)
+			// 	: inputCoin.value;
+
+			// inputCoin.value = inputCoinValue;
+			//#endregion
+
+			inputCur.value = (inputCoin.value * converterCurrentCurrency.currValue).toFixed(3);
+		});
+
+		inputCur.addEventListener('keyup', (e) => {
+			//#region THIS IS A RESTRICTION FOR MOBILE DEVICES BUT NEED TO FIX SOMETHING GETTING WRONG
+			// inputCur.value === '' && e.key === 'Backspace'
+			// 	? (inputCurValue = '')
+			// 	: inputCur.value !== ''
+			// 	? (inputCurValue = inputCur.value)
+			// 	: '';
+
+			// inputCur.value = inputCurValue;
+			//#endregion
+
+			inputCoin.value = (inputCur.value * converterCurrentCurrency.coinValue).toFixed(8);
+		});
+
+		const selectCoin = document.getElementById('select-coin');
+		const selectCurr = document.getElementById('select-curr');
+		const coinlist = document.querySelector('.coinlist');
+		const currlist = document.querySelector('.currlist');
+
+		selectCoin.addEventListener('click', () => {
+			coinlist.classList.toggle('d-flex');
+		});
+
+		selectCurr.addEventListener('click', () => {
+			currlist.classList.toggle('d-flex');
+		});
+	})();
 })();
 
 const loginButton = document.getElementById('logn');
@@ -124,65 +189,6 @@ cb.addEventListener('mousemove', (e) => {
 	}
 });
 
-const inputCoin = document.getElementById('inputcoin');
-const inputCur = document.getElementById('inputcur');
-inputCoin.min = 0;
-inputCur.min = 0;
-let inputCoinValue;
-let inputCurValue;
-
-//CONVERTER
-inputCoin.addEventListener('keydown', (e) => {
-	if (e.key === '-' || e.key === '+' || e.key === 'e' || e.key === 'E') {
-		e.preventDefault();
-	}
-});
-inputCur.addEventListener('keydown', (e) => {
-	if (e.key === '-' || e.key === '+' || e.key === 'e') {
-		e.preventDefault();
-	}
-});
-inputCoin.addEventListener('keyup', (e) => {
-	const currPrice = converterTop10.find((d) => d.id === 'bitcoin').current_price;
-
-	inputCoin.value === '' && e.key === 'Backspace'
-		? (inputCoinValue = '')
-		: inputCoin.value !== ''
-		? (inputCoinValue = inputCoin.value)
-		: '';
-
-	inputCoin.value = inputCoinValue;
-
-	inputCur.value = (inputCoin.value * currPrice).toFixed(3);
-});
-
-inputCur.addEventListener('keyup', (e) => {
-	const currPrice = converterTop10.find((d) => d.id === 'bitcoin').current_price;
-
-	inputCur.value === '' && e.key === 'Backspace'
-		? (inputCurValue = '')
-		: inputCur.value !== ''
-		? (inputCurValue = inputCur.value)
-		: '';
-
-	inputCur.value = inputCurValue;
-
-	inputCoin.value = (inputCur.value * (1 / currPrice)).toFixed(6);
-});
-
-const selectCoin = document.getElementById('select-coin');
-const selectCurr = document.getElementById('select-curr');
-const coinlist = document.querySelector('.coinlist');
-const currlist = document.querySelector('.currlist');
-
-selectCoin.addEventListener('click', () => {
-	coinlist.classList.toggle('d-flex');
-});
-
-selectCurr.addEventListener('click', () => {
-	currlist.classList.toggle('d-flex');
-});
-
 //JSX
 function addTopCurrencies(name, abr, curUS, pcp_24h, pc_24h, iconUrl, min, max, count) {
 	let priceChangeP = pcp_24h > 0 ? `+${pcp_24h}` : pcp_24h;
@@ -207,55 +213,81 @@ function addTopCurrencies(name, abr, curUS, pcp_24h, pc_24h, iconUrl, min, max, 
 		priceChange = ``;
 	}
 	const topcurr = `
-		<div class="topcur">
-			<div class="choiceoper">
-				<a href="#">more</a>
-			</div>
-            <div class="topcur-header">
-                <div class="topcur-header-img" style="background-image:url('${iconUrl}')"></div>
-                <h3>${name}</h3>
-                <h2>${abr.toUpperCase()}</h2>
-            </div>
-            <div class="topcur-body">
-                <h1>$${curUS}</h1>
-				<div class="p-change ${currentColor}">
-					<p>${priceChangeP}</p>
-					<span>${priceChange}</span>
-				</div>
-            </div>
-			<div class="topcur-foot">
-				<div class="topcur-foot-inf max">
-                    <p>$${max}</p>
-                    <span>max | 24h</span>
-                </div>
-                <div class="topcur-foot-inf min">
-                    <p>$${min}</p>
-                    <span>min | 24h</span>
-                </div>
-			</div>
-			<div class="topcur-pager"><p>${count}</p></div>
-        </div>`;
+				<div class="topcur">
+					<div class="choiceoper">
+						<a href="#">more</a>
+					</div>
+					<div class="topcur-header">
+						<div class="topcur-header-img" style="background-image:url('${iconUrl}')"></div>
+						<h3>${name}</h3>
+						<h2>${abr.toUpperCase()}</h2>
+					</div>
+					<div class="topcur-body">
+						<h1>$${curUS}</h1>
+						<div class="p-change ${currentColor}">
+							<p>${priceChangeP}</p>
+							<span>${priceChange}</span>
+						</div>
+					</div>
+					<div class="topcur-foot">
+						<div class="topcur-foot-inf max">
+							<p>$${max}</p>
+							<span>max | 24h</span>
+						</div>
+						<div class="topcur-foot-inf min">
+							<p>$${min}</p>
+							<span>min | 24h</span>
+						</div>
+					</div>
+					<div class="topcur-pager"><p>${count}</p></div>
+				</div>`;
 
 	return topcurr;
 }
 
-function addConverter({ coinName, coinSymbol, coinValue, currSymbol, currValue }) {
+function addConverter({ coinName, coinSymbol, coinValue, currSymbol, currValue, coinIco, sparkline }) {
 	const converter = `
-	<div class="converter-icon"></div>
-    <div class="converter-header">
-        <h3>1 BTC = 20239 USD</h3>
-    </div>
-    <div class="converter-header">
-        <h3>1 USD = 0.00004715 BTC</h3>
-    </div>
-    <div class="converter-body">
-        <div class="input-group">
-            <label id="convcoin" for="inputcoin">BTC<span>▼</span></label>
-            <input id="inputcoin" type="number">
-        </div>
-        <div class="input-group">
-            <label id="convcur" for="inputcur">USD<span>▼</span></label>
-            <input id="inputcur" type="number">
-        </div>
-    </div>`;
+				<div class="converter-icon" style="background-image: url('${coinIco}')"></div>
+                    <div class="converter-header">
+                        <h3>1 ${coinSymbol} = ${currValue} ${currSymbol}</h3>
+                    </div>
+                    <div class="converter-header">
+                        <h3>1 ${currSymbol} = ${coinValue.toFixed(6)} ${coinSymbol}</h3>
+                    </div>
+                    <div class="converter-body">
+                        <div class="group-collection">
+                            <div class="input-group">
+                                <label id="select-coin" for="">BTC<span>▼</span></label>
+                                <input id="inputcoin" type="number">
+                            </div>
+                            <div class="item-list coinlist">
+                                <input type="text" placeholder="Search coin">
+                                <ul>
+                                    <li>Bitcoin - BTC</li>
+                                    <li>Bitcoin - BTC</li>
+                                    <li>Bitcoin - BTC</li>
+                                    <li>Bitcoin - BTC</li>
+                                    <li>Bitcoin - BTC</li>
+                                    <li>Bitcoin - BTC</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="group-collection">
+                            <div class="input-group">
+                                <label id="select-curr" for="">USD<span>▼</span></label>
+                                <input id="inputcur" type="number">
+                            </div>
+                            <div class="item-list text-center currlist">
+                                <ul>
+                                    <li>USD</li>
+                                    <li>USD</li>
+                                    <li>USD</li>
+                                    <li>USD</li>
+                                    <li>USD</li>
+                                    <li>USD</li>
+                                </ul>
+                            </div>
+                        </div>
+                </div>`;
+	return converter;
 }
